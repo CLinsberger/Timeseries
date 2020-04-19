b5ddc639670d29371b741acffc4728510c43f7c8
CLinsberger <c.linsberger@gmail.com>
2020-04-19 17:07:34 +0200

    Update analyse.py

--- download/analyse.py
+++ download/analyse.py
@@ -8,19 +8,50 @@ from pathlib import Path
 
 path_data = '../data/locations'
 
+df_center_meanbands = pd.DataFrame()
+
 for folder in os.listdir(path_data):
     path_csv = path_data + '/' + folder
 
-    df_center_meanbands = pd.DataFrame(columns=['MIN', 'MAX'])
-    i = 0
+
     for item in os.listdir(path_csv):
 
         if item.lower().endswith("_center_meanbands.csv"):
             path = path_csv + '/' + item
 
             data = pd.read_csv(path, sep=';')
-            df_center_meanbands = df_center_meanbands.append(pd.Series([data['MIN'], data['MAX']], index = i), )
-            i = i + 1
+            min = int(data['MIN'])
+            max = int(data['MAX'])
+
+            start = item.find("_") + len("_")
+            end = item.find("_center_meanbands")
+            year = int(item[start:end])
+
+            df_temp = pd.DataFrame([[min, max, year]])
+            df_center_meanbands = df_center_meanbands.append(df_temp, ignore_index=True)
 
 
-print(df_center_meanbands)
+count_min_smaller = 0
+count_min_bigger = 0
+
+count_max_smaller = 0
+count_max_bigger = 0
+
+for location in df_center_meanbands.itertuples():
+    print(location)
+    if (location[1] < location[3]):
+        count_min_smaller = count_min_smaller + 1
+
+    if (location[1] > location[3]):
+        count_min_bigger = count_min_bigger + 1
+
+
+    if (location[2] < location[3]):
+        count_max_smaller = count_max_smaller + 1
+
+    if (location[2] > location[3]):
+        count_max_bigger = count_max_bigger + 1
+
+
+print(count_min_smaller, count_min_bigger)
+print(count_max_smaller, count_max_bigger)
